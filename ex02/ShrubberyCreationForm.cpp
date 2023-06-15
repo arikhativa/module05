2566b8558a98442ad8fbcc25ba0b2c4c63d75be2
Arik Hativa <arikhativa@gmail.com>
2023-06-15 11:04:02 +0300

    mid

--- ex02/ShrubberyCreationForm.cpp
+++ ex02/ShrubberyCreationForm.cpp
@@ -5,13 +5,15 @@
 */
 
 ShrubberyCreationForm::ShrubberyCreationForm()
+	:	AForm("ShrubberyCreationForm", 145, 137)
 {
 }
 
 ShrubberyCreationForm::ShrubberyCreationForm( const ShrubberyCreationForm & src )
+	:	AForm("ShrubberyCreationForm", 145, 137)
 {
+	(void)src;
 }
-
 
 /*
 ** -------------------------------- DESTRUCTOR --------------------------------
@@ -21,31 +23,32 @@ ShrubberyCreationForm::~ShrubberyCreationForm()
 {
 }
 
-
 /*
 ** --------------------------------- OVERLOAD ---------------------------------
 */
 
 ShrubberyCreationForm &				ShrubberyCreationForm::operator=( ShrubberyCreationForm const & rhs )
 {
-	//if ( this != &rhs )
-	//{
-		//this->_value = rhs.getValue();
-	//}
+	(void)rhs;
 	return *this;
 }
 
 std::ostream &			operator<<( std::ostream & o, ShrubberyCreationForm const & i )
 {
-	//o << "Value = " << i.getValue();
+	AForm const *a = &i;
+	o << a;
 	return o;
 }
-
 
 /*
 ** --------------------------------- METHODS ----------------------------------
 */
 
+void		ShrubberyCreationForm::execute(Bureaucrat const & executor) const
+{
+	_canExecForm(executor);
+
+}
 
 /*
 ** --------------------------------- ACCESSOR ---------------------------------
--- ex02/ShrubberyCreationForm.hpp
+++ ex02/ShrubberyCreationForm.hpp
@@ -1,21 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ShrubberyCreationForm.hpp                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yrabby <yrabby@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/15 10:57:51 by yrabby            #+#    #+#             */
+/*   Updated: 2023/06/15 10:59:04 by yrabby           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef SHRUBBERYCREATIONFORM_HPP
 # define SHRUBBERYCREATIONFORM_HPP
 
 # include <iostream>
 # include <string>
 
-class ShrubberyCreationForm
+# include "AForm.hpp"
+
+class ShrubberyCreationForm : public AForm
 {
 
 	public:
-
 		ShrubberyCreationForm();
-		ShrubberyCreationForm( ShrubberyCreationForm const & src );
-		~ShrubberyCreationForm();
-
-		ShrubberyCreationForm &		operator=( ShrubberyCreationForm const & rhs );
+		virtual ~ShrubberyCreationForm();
+		virtual void		execute(Bureaucrat const & executor) const;
 
 	private:
+		ShrubberyCreationForm( ShrubberyCreationForm const & src );
+		ShrubberyCreationForm &		operator=( ShrubberyCreationForm const & rhs );
 
 };
 
